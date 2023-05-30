8b1976118be16bf73fbdb0764597c921a55ba971
Chunnien Chan <121328115+chunnienc@users.noreply.github.com>
2023-05-29 17:04:28 -0700

    Add script

--- utils/bazel/run_torchscript_to_tf_naive.sh
+++ utils/bazel/run_torchscript_to_tf_naive.sh
@@ -5,14 +5,18 @@ PASS_PIPELINE="
     builtin.module(
         torchscript-module-to-torch-backend-pipeline,
         torch-backend-to-stablehlo-backend-pipeline,
-        odml-print-op-stats,
+        # odml-print-op-stats,
         stablehlo-legalize-to-hlo,
         func.func(chlo-legalize-to-hlo),
         func.func(tf-legalize-hlo),
-        odml-print-op-stats
+        print-op-stats
     )"
+
+
+PASS_PIPELINE_NO_COMMENTS=$(sed -E 's/\#.*|(\/\/).*//g' <<< "$PASS_PIPELINE")
+PASS_PIPELINE_NO_COMMENTS_SPACES=$(echo $PASS_PIPELINE_NO_COMMENTS | sed 's/ //g')
 
 bazel run @torch-mlir//:torch-mlir-opt -- \
     /models/resnet18_raw.mlir \
     -o /models/test.mlir \
-    -pass-pipeline="$(echo $PASS_PIPELINE | sed 's/ //g')"
+    -pass-pipeline="$PASS_PIPELINE_NO_COMMENTS_SPACES"
