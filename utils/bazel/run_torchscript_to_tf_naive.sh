b9c10425c93cdf1d5d1ffbfb5775f74f7017fce0
Chunnien Chan <121328115+chunnienc@users.noreply.github.com>
2023-05-31 13:20:31 -0700

    Output debug locations

--- utils/bazel/run_torchscript_to_tf_naive.sh
+++ utils/bazel/run_torchscript_to_tf_naive.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
-# PASS_PIPELINE="
-#     builtin.module(
-#         torchscript-module-to-torch-backend-pipeline,
-#         ###### Print torch ops ######
-#         print-op-stats,
-#         # func.func(convert-torch-to-stablehlo),
-#         torch-backend-to-stablehlo-backend-pipeline,
-#         stablehlo-legalize-to-hlo,
-#         func.func(chlo-legalize-to-hlo),
-#         func.func(tf-legalize-hlo),
-#         ###### Print all converted ops ######
-#         print-op-stats,
-#     )"
+OUTPUT_FILENAME=/models/test.mlir
+
+OUTPUT_NAME=$(sed -E 's/(.mlir)?$//g' <<< $OUTPUT_FILENAME)
+OUTPUT_FILENAME=$(sed -E 's/(.mlir)?$/.mlir/g' <<< $OUTPUT_FILENAME)
 
 PASS_PIPELINE="
     builtin.module(
         torchscript-module-to-torch-backend-pipeline,
-        # torch-backend-to-stablehlo-backend-pipeline,
-        func.func(torch-backend-to-tf),
-        # func.func(tf-legalize-hlo),
+        snapshot-op-locations {tag=torch filename=${OUTPUT_NAME}_torch.mlir)},
+        ###### Print torch ops ######
+        print-op-stats,
+        torch-backend-to-stablehlo-backend-pipeline,
+        snapshot-op-locations {tag=stablehlo filename=${OUTPUT_NAME}_stablehlo.mlir)},
+        stablehlo-legalize-to-hlo,
+        func.func(chlo-legalize-to-hlo),
+        snapshot-op-locations {tag=hlo filename=${OUTPUT_NAME}_hlo.mlir)},
+        func.func(tf-legalize-hlo),
+        ###### Print TF/HLO ops ######
         print-op-stats,
     )"
 
-
+PASS_PIPELINE_RAW=$PASS_PIPELINE
 # Remove comment lines
-PASS_PIPELINE=$(sed -E 's/\#.*|(\/\/).*//g' <<< "$PASS_PIPELINE")
-# Remove spaces
-PASS_PIPELINE=$(echo $PASS_PIPELINE | sed 's/ //g')
+PASS_PIPELINE=$(sed -E 's/\#.*|(\/\/).*//g' <<< $PASS_PIPELINE)
 # Remove trailing commas
-PASS_PIPELINE=$(echo $PASS_PIPELINE | sed 's/,)/)/g')
+PASS_PIPELINE=$(echo $PASS_PIPELINE | sed -E 's/,\s+\)/)/g')
 
 bazel run @torch-mlir//:torch-mlir-opt -- \
     /models/resnet18_raw.mlir \
-    -o /models/test.mlir \
-    -pass-pipeline="$PASS_PIPELINE"
+    -o $OUTPUT_FILENAME \
+    -pass-pipeline="$PASS_PIPELINE" \
+    -mlir-print-debuginfo
+
+echo "$PASS_PIPELINE_RAW"
